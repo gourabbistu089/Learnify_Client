48f77bac9b607176f407741e4b7fd997ca34e1e6
Gourab <gourabbistu2002@gmail.com>
2025-03-17 18:57:02 +0530

    Fix Vite routing on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensure correct routing
+  server: {
+    historyApiFallback: true, // Fix 404 on refresh
+  }
 })
